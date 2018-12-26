85360ed232748fce9eb25d4e1374a3ce88afb82d
penzai <pengzhen058@qq.com>
2018-12-26 16:35:21 +0800

    feat: add feature
    aspect-ratio
    px2vw

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,22 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
+    autoprefixer: {},
+    "postcss-aspect-ratio-mini": {},
+    "postcss-px-to-viewport": {
+      // 基准 - 视窗宽度
+      viewportWidth: 750,
+      // 转换后单位所带的小数点位数
+      unitPrecision: 5,
+      // 转换后单位
+      viewportUnit: "vw",
+      // 转换后字体的单位
+      fontViewportUnit: "vmin", // ???暂时无效
+      // 忽略的样式名
+      selectorBlackList: [".ignore", ".hairlines"],
+      // 最小px
+      minPixelValue: 1,
+      // 是否转换媒体查询的px单位
+      mediaQuery: false
+    }
   }
 };
