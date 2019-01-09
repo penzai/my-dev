9eeb0f697f4b1f6503f465e113f87beb692cf198
penzai <pengzhen058@qq.com>
2019-01-09 17:35:27 +0800

    fix(less): less调整
    去掉无用组件
    注入mixins

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,10 @@ const path = require("path");
 module.exports = {
   pluginOptions: {
     "style-resources-loader": {
-      patterns: [path.resolve(__dirname, "src/styles/variables.less")],
+      patterns: [
+        path.resolve(__dirname, "src/styles/variables.less"),
+        path.resolve(__dirname, "src/styles/mixins.less")
+      ],
       preProcessor: "less"
     }
   }
